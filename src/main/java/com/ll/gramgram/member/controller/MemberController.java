8364ed78c31f42cbeac120a865d2cd0a5c497944
uoise <yw823x@daum.net>
2023-03-30 09:44:13 +0900

    MemberControllerTests added

---
+++ src/main/java/com/ll/gramgram/member/controller/MemberController.java
@@ -0,0 +1,17 @@
+package com.ll.gramgram.member.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@RequiredArgsConstructor
+public class MemberController {
+
+    @GetMapping("/join")
+    public String showJoin() {
+        return "showJoin";
+    }
+}
---
+++ src/test/java/com/ll/gramgram/member/controller/MemberControllerTests.java
@@ -0,0 +1,40 @@
+package com.ll.gramgram.member.controller;
+
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.handler;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+@ActiveProfiles("test")
+public class MemberControllerTests {
+
+    @Autowired
+    private MockMvc mvc;
+
+    @Test
+    @DisplayName("h1")
+    void t001() throws Exception {
+        // Controller 테스트는 통합 테스트
+        ResultActions resultActions = mvc
+                .perform(get("/member/join"))
+                .andDo(print());
+
+        resultActions
+                .andExpect(handler().handlerType(MemberController.class))
+                .andExpect(handler().methodName("showJoin"))
+                .andExpect(status().is2xxSuccessful());
+    }
+}
