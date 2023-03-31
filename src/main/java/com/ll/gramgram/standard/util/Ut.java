d377d14c87f56b8e5dccdba9db32af16c8f601aa
Youngwoo Choi <yw823x@daum.net>
2023-03-31 14:49:38 +0900

    redirects

--- src/main/java/com/ll/gramgram/standard/util/Ut.java
+++ src/main/java/com/ll/gramgram/standard/util/Ut.java
@@ -8,5 +8,41 @@ public class Ut {
         public static String encode(String s) {
             return URLEncoder.encode(s, StandardCharsets.UTF_8);
         }
+
+        public static String modifyQueryParam(String url, String paramName, String paramValue) {
+            url = deleteQueryParam(url, paramName);
+            url = addQueryParam(url, paramName, paramValue);
+
+            return url;
+        }
+    }
+
+    public static String addQueryParam(String url, String paramName, String paramValue) {
+        if (!url.contains("?")) {
+            url += "?";
+        }
+
+        if (!url.endsWith("?") && !url.endsWith("&")) {
+            url += "&";
+        }
+
+        url += paramName + "=" + paramValue;
+
+        return url;
+    }
+
+    private static String deleteQueryParam(String url, String paramName) {
+        int startPoint = url.indexOf(paramName + "=");
+        if (startPoint == -1) return url;
+
+        int endPoint = url.substring(startPoint).indexOf("&");
+
+        if (endPoint == -1) {
+            return url.substring(0, startPoint - 1);
+        }
+
+        String urlAfter = url.substring(startPoint + endPoint + 1);
+
+        return url.substring(0, startPoint) + urlAfter;
     }
 }
